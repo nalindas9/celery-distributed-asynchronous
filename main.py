e31c84ec8fb0ea8640f73722f32e8b841aa55d0c
Nalin Das <nalindas9@gmail.com>
2022-04-11 20:14:40 -0700

    Stiched frames from both cams

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import cv2
 if __name__ == '__main__':
     # Use celery worker to process images
     # Webcam
-    result_camera1 = processImageFirstCamera.delay(0)
+    result_camera1 = semanticSegmentation.delay([0, 2])
     # External webcam
-    result_camera2 = processImageSecondCamera.delay(2)
+    #result_camera2 = process.delay(2)
--- tasks.py
+++ tasks.py
@@ -8,24 +8,34 @@ app = Celery('tasks',
              broker='amqp://guest@localhost//',
              backend='db+sqlite:///db.sqlite3')
 
-@app.task(queue='first_camera')
-def processImageFirstCamera(cam_id):
-    print('Processing video from camera {}'.format(cam_id))
-    # Display video from camera
-    cap = cv2.VideoCapture(cam_id)
+@app.task(queue='semantic_segmentation')
+def semanticSegmentation(cam_ids):
+    print('Semantic segmentation task started')
+    print('Camera IDs:', cam_ids)
+    # Video capture object for the cameras
+    cap = [cv2.VideoCapture(cam_id) for cam_id in cam_ids]
+    print('Video capture objects created: {}'.format(cap))
+    # Display video for the cameras
     while True:
-        ret, frame = cap.read()
-        if ret:
-            cv2.imshow('frame', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
+        # Read the frames
+        frames = [cap[i].read()[1] for i in range(len(cap))]
+        # Stitch the frames horizontally
+        stitched = cv2.hconcat(frames)
+        # Show the stitched frames
+        cv2.imshow('Semantic segmentation', stitched)
+        # Wait for key press
+        key = cv2.waitKey(1)
+        # Stop if 'q' is pressed
+        if key == ord('q'):
             break
-    cap.release()
+    # Release the video capture objects
+    for cap in cap:
+        cap.release()
+    # Destroy all windows
     cv2.destroyAllWindows()
 
 @app.task(queue='second_camera')
-def processImageSecondCamera(cam_id):
+def process(cam_id):
     print('Processing video from camera {}'.format(cam_id))
     # Display video from camera
     cap = cv2.VideoCapture(cam_id)
